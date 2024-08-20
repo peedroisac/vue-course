3fe44ff481582143d2d972e269bc90df644c11ab
peedroisac <peedroisac@gmail.com>
2024-08-20 13:07:10 -0300

    primeiro commit na main

---
+++ app/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+
+  <TheHeader v-if="showHeader"/>
+
+  <div v-show="showName">
+    Nome: {{ firstName }}<br>
+    Sobrenome: {{ lastName }}
+  </div>
+
+  <div v-if="accessLevel === 'admin'">Usuário Admin</div>
+  <div v-else-if="accessLevel === 'marketing'">Usuário Marketing</div>
+  <div v-else=>Usuário normal</div>
+
+  <img
+
+      alt="Vue logo"
+      src="./assets/logo.png"
+  >
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/TheHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    TheHeader
+  },
+  data() {
+    return{
+      showHeader: true,
+      firstName: 'Jon',
+      lastName: 'Snow',
+      showName: false,
+      accessLevel: 'Admin',
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ app/src/components/TheHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <header class="header">
+    Header
+    </header>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style>
+  .header {
+    background: #000;
+    color: #fff;
+  }
+</style>
